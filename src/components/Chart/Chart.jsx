edff64712880fbc262e6b30b810b6f1f8744d159
nomiboy <noumanijaz.dev@outlook.com>
2022-01-21 12:20:40 +0500

    Finished

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,14 +3,17 @@ import "./Chart.css";
 import ChartBar from "./ChartBar";
 
 function Chart(props) {
+	const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+	const totalMaximum = Math.max(...dataPointValues);
+
 	return (
 		<div className="chart">
 			{props.dataPoints.map((dataPoint) => (
-                <ChartBar
-                    key={dataPoint.label}
+				<ChartBar
+					key={dataPoint.label}
 					value={dataPoint.value}
-					maxValue={null}
-					lebel={dataPoint.label}
+					maxValue={totalMaximum}
+					label={dataPoint.label}
 				/>
 			))}
 		</div>
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -4,6 +4,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 // Controll or statefull Component
 function Expenses({ items }) {
@@ -23,6 +24,7 @@ function Expenses({ items }) {
 				selected={filteredYear}
 				onChangeFilter={filterChangeHandler}
 			/>
+			<ExpensesChart expenses={filteredExpense}/>
 			<ExpensesList items={filteredExpense} />
 		</Card>
 	);
